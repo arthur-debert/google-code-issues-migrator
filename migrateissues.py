99823f3627ec025e28ec4e50d7e953806a8bbccf
Joel Thornton <github@joelpt.net>
2012-03-21 02:43:28 -0700

    Further lower Github req/sec rate to 0.50 to try and avoid API rate limit error.

--- migrateissues.py
+++ migrateissues.py
@@ -93,7 +93,7 @@ def download_issues():
 
 def post_to_github(issue, sync_comments=True):
     logging.info('should post %s', issue)
-    github = Github(username=options.github_user_name, api_token=options.github_api_token, requests_per_second=0.75)
+    github = Github(username=options.github_user_name, api_token=options.github_api_token, requests_per_second=0.50)
     if issue.status.lower()  in "invalid closed fixed wontfix verified".lower():
         issue.status = 'closed'
     else:
