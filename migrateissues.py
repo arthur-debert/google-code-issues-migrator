e118fb2adc607d0cb363cff36093ec088ae01608
Marc Abramowitz <marc@marc-abramowitz.com>
2013-09-20 08:57:43 -0700

    Fix for error when running with --dry-run: "UnboundLocalError: local variable 'github_issue' referenced before assignment"

--- migrateissues.py
+++ migrateissues.py
@@ -93,6 +93,8 @@ def add_issue_to_github(issue):
 
     output('Adding issue %d' % issue['gid'])
 
+    github_issue = None
+
     if not options.dry_run:
         github_labels = [github_label(label) for label in issue['labels']]
         github_issue = github_repo.create_issue(issue['title'], body = body.encode('utf-8'), labels = github_labels)
